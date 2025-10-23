9e946ba5f1955aece1d4befbbdb8b95c9e57e533
lara-isabella <laraisabellaregina@gmail.com>
2025-10-23 01:41:24 -0300

    Avances CollectionUsuario

--- Documents/E2025POO/tp6_grupo7/src/ar/edu/unju/escmi/tp6/principal/Principal.java
+++ Documents/E2025POO/tp6_grupo7/src/ar/edu/unju/escmi/tp6/principal/Principal.java
@@ -1,30 +1,53 @@
 package ar.edu.unju.escmi.tp6.principal;
 
 import java.util.Scanner;
-import ar.edu.unju.escmi.tp6.collections.*;
-import ar.edu.unju.escmi.tp6.dominio.*;
+import ar.edu.unju.escmi.tp6.collections.* ;
+import ar.edu.unju.escmi.tp6.dominio.* ;
 
 public class Principal {
-    
-    static Scanner sc = new Scanner(System.in);
 
-    byte op;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte op;
 
-    do {
-        System.out.println("\n******** MENU ********");
-        System.out.println("1 - Registrar libro.");
-        System.out.println("2 - Registrar usuario.");
-        System.out.println("3 - Préstamo de libro.");
-        System.out.println("4 - Devolución de libro.");
-        System.out.println("5 - Listar libros.");
-        System.out.println("6 - Salir");        
-        System.out.print("\n\n Ingrese una opción: ");
-        op = sc.nextByte();
-        sc.nextLine();
+        do{
+            System.out.println("\n******** MENU ********");
+            System.out.println("1 - Registrar libro.");
+            System.out.println("2 - Registrar usuario.");
+            System.out.println("3 - Préstamo de libro.");
+            System.out.println("4 - Devolución de libro.");
+            System.out.println("5 - Listar libros.");
+            System.out.println("6 - Salir");        
+            System.out.print("\n\n Ingrese una opción: ");
+            op = sc.nextByte();
 
-        switch (op) {
+            switch (op) {
+                case 1:
+                    System.out.println("Opción 1 seleccionada: REGISTRAR LIBRO");
+                    break;
+                case 2:
+                    System.out.println("Opción 2 seleccionada: REGISTRAR USUARIO");
+                    break;
+                case 3:
+                    System.out.println("Opción 3 seleccionada: PRESTAMO DE UN LIBRO");
+                    break;
+                case 4:
+                    System.out.println("Opción 4 seleccionada: DEVOLUCION DE UN LIBRO");
+                    break;
+                case 5:
+                    System.out.println("Opción 5 seleccionada: LISTAR LIBROS");
+                    break;
+                case 6: 
+                    System.out.println("Opción 6 seleccionada: SALIR DEL SISTEMA");
+                    System.out.println("Saliendo del sistema. \n Saliendo del sistema.. \n Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opción no válida seleccionada: INTENTA DE NUEVO");
+            }
 
-        }
-    } while (op != 6);
+            sc.close();
+        } while (op != 6);
+        
+    }
 
 }
